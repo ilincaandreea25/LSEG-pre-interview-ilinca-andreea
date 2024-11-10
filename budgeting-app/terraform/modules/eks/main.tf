819f11c6097af3ce3911391de841285936c6cf44
ilincaandreea25 <ilinca.andreea00@gmail.com>
2024-11-10 18:58:56 +0200

    File commit terraform apply

--- budgeting-app/terraform/modules/eks/main.tf
+++ budgeting-app/terraform/modules/eks/main.tf
@@ -1,14 +1,3 @@
-resource "aws_iam_role" "eks_role" {
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17",
-    Statement = [{
-      Action    = "sts:AssumeRole"
-      Principal = { Service = "eks.amazonaws.com" }
-      Effect    = "Allow"
-    }]
-  })
-}
-
 resource "aws_eks_cluster" "eks" {
   name     = "budgeting-app-cluster"
   role_arn = aws_iam_role.eks_role.arn
@@ -18,17 +7,51 @@ resource "aws_eks_cluster" "eks" {
   }
 }
 
-resource "aws_iam_role" "worker_role" {
+resource "aws_iam_role" "eks_role" {
   assume_role_policy = jsonencode({
     Version = "2012-10-17",
-    Statement = [{
-      Action    = "sts:AssumeRole"
-      Principal = { Service = "ec2.amazonaws.com" }
-      Effect    = "Allow"
-    }]
+    Statement = [
+      {
+        Action    = "sts:AssumeRole",
+        Effect    = "Allow",
+        Principal = { Service = "eks.amazonaws.com" }
+      }
+    ]
   })
 }
 
+
+resource "aws_iam_role" "worker_role" {
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Action    = "sts:AssumeRole",
+        Effect    = "Allow",
+        Principal = { Service = "ec2.amazonaws.com" }
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "eks_worker_node_policy" {
+  role       = aws_iam_role.worker_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
+}
+
+# AmazonEKS_CNI_Policy (required for networking)
+resource "aws_iam_role_policy_attachment" "eks_cni_policy" {
+  role       = aws_iam_role.worker_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
+}
+
+# AmazonEC2ContainerRegistryReadOnly (to pull images from ECR)
+resource "aws_iam_role_policy_attachment" "ec2_container_registry_read_only" {
+  role       = aws_iam_role.worker_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
+}
+
+# Define EKS Node Group
 resource "aws_eks_node_group" "node_group" {
   cluster_name    = aws_eks_cluster.eks.name
   node_role_arn   = aws_iam_role.worker_role.arn
@@ -38,12 +61,5 @@ resource "aws_eks_node_group" "node_group" {
     max_size     = 3
     min_size     = 1
   }
-}
-
-output "cluster_name" {
-  value = aws_eks_cluster.eks.name
-}
-
-output "node_group_arn" {
-  value = aws_eks_node_group.node_group.arn
+  ami_type = "AL2_x86_64"  # Use Amazon EKS-optimized Amazon Linux 2 AMI
 }
